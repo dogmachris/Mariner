a9cb3846efd910fb1c46cd6a67a4989fb3c96b24
dogmachris <chris.dogma@googlemail.com>
2024-06-03 22:41:18 +0200

    Add files via upload

---
+++ AQUAS Evo/Scripts/Controllers/AE_OceanController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AquasEvo
+{
+    [ExecuteInEditMode]
+    public class AE_OceanController : MonoBehaviour
+    {
+
+    }
+}
